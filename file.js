c5d3801446bd58d72c5674b723b158726b42db1e
Sarah Stevenson <43302656+sas132@users.noreply.github.com>
2019-11-26 15:53:30 -0600

    Update file.js

--- file.js
+++ file.js
@@ -1,17 +1,17 @@
+console.log("Hello World");
 var readline = require('readline');
-
 var prompts = readline.createInterface(process.stdin, process.stdout);
 
     var version = "This is version 1 of the HotBurger service";
-    let totalProfit = 0;
-    let topSellerStr = "";
-    let topSellerNum = 0;
+    var totalProfit = 0;
+    var topSellerStr = "";
+    var topSellerNum = 0;
     var hotdogSales = 0;
     var hamSales = 0;
     var sodaSales = 0;
     var cookieSales = 0;
     var requestCount = 0;
-    let lastHappened = true;
+    var lastHappened = true;
 
     var menuName = [];
     menuName.push("Hotdog");
@@ -28,8 +28,8 @@ var prompts = readline.createInterface(process.stdin, process.stdout);
   continuslyAsk();
 
     function continuslyAsk(){
-      prompts.question(`$ `, function (input){
-          
+      prompts.question("$ ", function (input){
+
         if(input == "/version")
         {
             console.log(version);
@@ -40,24 +40,24 @@ var prompts = readline.createInterface(process.stdin, process.stdout);
         else if(input == "/getmenu")
         {
             requestCount++;
-            let i = 0;
+            var i = 0;
             for(i = 0; i < 4; i++)
             {
-                console.log(`${menuName[i]}: $${menuPrice[i]}`);
+                console.log(menuName[i] + ": $" + menuPrice[i]);
             }
             lastHappened = true;
             continuslyAsk();
         }
         else if(input == "/gettotal")
         {
-            console.log(`Total revenue: ${totalProfit}`);
+            console.log("Total revenue: " + totalProfit);
             requestCount++;
             lastHappened = true;
             continuslyAsk();
         }
         else if(input == "/gettopseller")
         {
-            console.log(`Top seller: ${topSellerStr} x${topSellerNum}`);
+            console.log("Top seller: " + topSellerStr + "x" + topSellerNum);
             requestCount++;
             lastHappened = true;
             continuslyAsk();
@@ -69,12 +69,64 @@ var prompts = readline.createInterface(process.stdin, process.stdout);
             lastHappened = true;
             continuslyAsk();
         }
+        else if(input.substring(0, 9) == "/getcount")
+        {
+          requestCount++;
+          if(input.substring(9, 16) == "/Hotdog")
+          {
+              console.log("Number of Hotdog sold: " + HotdogSales);
+          }
+          else if(input.substring(9, 16) == "/Cookie")
+          {
+              console.log("Number of Cookie sold: " + CookieSales);
+          }
+          else if(input.substring(9, 14) == "/Soda")
+          {
+              console.log("Number of Soda sold: " + SodaSales);
+          }
+          else if(input.substring(9, 19) == "/Hamburger")
+          {
+              console.log("Number of Hamburger sold: " + HamburgerSales);
+          }
+          else
+          {
+              console.log(input + " is an invalid command.");
+          }
+          continuslyAsk();
+        }
+        else if(input.substring(0, 9) == "/setcount")
+        {
+          if(input.substring(9, 16) == "/Hotdog")
+          {
+              var tempNum = parseInt(input.substring(17, input.length));
+              HotdogSales = tempNum;
+          }
+          else if(input.substring(9, 16) == "/Cookie")
+          {
+            var tempNum = parseInt(input.substring(17, input.length));
+              CookieSales = tempNum;
+          }
+          else if(input.substring(9, 14) == "/Soda")
+          {
+            var tempNum = parseInt(input.substring(15, input.length));
+            SodaSales = tempNum;
+          }
+          else if(input.substring(9, 19) == "/Hamburger")
+          {
+            var tempNum = parseInt(input.substring(20, input.length));
+            HamburgerSales = tempNum;
+          }
+          else
+          {
+              console.log(input + " is an invalid command.");
+          }
+        }
         else if(input.substring(0, 9) == "/purchase")
         {
             requestCount++;
             if(input.substring(9, 16) == "/Hotdog")
             {
-                let tempHot = parseInt(input.substring(17, input.length));
+                var tempHot = parseInt(input.substring(17, input.length));
                 hotdogSales += tempHot;
                 totalProfit += (tempHot * menuPrice[0]);
                 if(topSellerNum < hotdogSales)
@@ -86,7 +138,7 @@ var prompts = readline.createInterface(process.stdin, process.stdout);
             }
             else if(input.substring(9, 16) == "/Cookie")
             {
-                let tempCookie = parseInt(input.substring(17, input.length));
+                var tempCookie = parseInt(input.substring(17, input.length));
                 cookieSales += tempCookie;
                 totalProfit += (tempCookie * menuPrice[3]);
                 if(topSellerNum < cookieSales)
@@ -98,7 +150,7 @@ var prompts = readline.createInterface(process.stdin, process.stdout);
             }
             else if(input.substring(9, 14) == "/Soda")
             {
-                let tempSoda = parseInt(input.substring(15, input.length));
+                var tempSoda = parseInt(input.substring(15, input.length));
                 sodaSales += tempSoda;
                 totalProfit += (tempSoda * menuPrice[2]);
                 if(topSellerNum < sodaSales)
@@ -110,7 +162,7 @@ var prompts = readline.createInterface(process.stdin, process.stdout);
             }
             else if(input.substring(9, 19) == "/Hamburger")
             {
-                let tempHam = parseInt(input.substring(20, input.length));
+                var tempHam = parseInt(input.substring(20, input.length));
                 hamSales += tempHam;
                 totalProfit += (tempHam * menuPrice[1]);
                 if(topSellerNum < hamSales)
@@ -123,7 +175,7 @@ var prompts = readline.createInterface(process.stdin, process.stdout);
             else
             {
                 requestCount++;
-                console.log(`${input} is an invalid command.`);
+                console.log(input + " is an invalid command.");
                 lastHappened = false;
             }
             continuslyAsk();
@@ -136,7 +188,7 @@ var prompts = readline.createInterface(process.stdin, process.stdout);
         }
         else
         {
-            console.log(`${input} is an invalid command.`);
+            console.log(input + " is an invalid command.");
             lastHappened = false;
             continuslyAsk();
         }
